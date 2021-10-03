f3c72ac9d2e73f93f7eabe57d16166a014c9a495
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-04 00:24:01 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'Allu_ArjunROBOTv3)
+SESSION = environ.get('SESSION', 'Allu_ArjunROBOTv3')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
