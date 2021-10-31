511c84f9f941ea221ebddaa57a83a4485dd9b7fa
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-31 11:39:27 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -126,8 +126,8 @@ async def group(client, message):
                 file_id = file.file_id
                 filename = f"{file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?=pr0fess0r_99_-_-_-_"),
-                     InlineKeyboardButton(text=f"[{get_size(file.file_size)}]", url=f"https://telegram.dog/{nyva}?=pr0fess0r_99_-_-_-_")]
+                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?Start=pr0fess0r_99_-_-_-_"),
+                     InlineKeyboardButton(url=f"https://telegram.dog/{nyva}?Start=pr0fess0r_99_-_-_-_")]
                 )
         else:
             return
